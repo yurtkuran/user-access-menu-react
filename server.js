8c3869653be0a7d1be2edd73186ce74cc53822e7
Erol Yurtkuran <erol@yurtkuran.net>
2020-04-26 14:14:48 -0700

    user and auth routes

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
 
 // bring in local modules
+const connectDB = require('./config/db');
 
 // clear console
 console.clear();
 
 // connect to database
+connectDB();
 
 // initalize app
 const app = express();
 
 // body parser middleware
+app.use(express.json({ extended: false }));
 
 // inital route
 // app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 app.get('/', (req, res) => res.send('api running'));
 
 // define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/profile', require('./routes/profile'));
+app.use('/api/posts', require('./routes/posts'));
 
 // server static assets in production
 if (process.env.NODE_ENV === 'production') {
